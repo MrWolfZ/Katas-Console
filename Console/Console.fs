a2ae5044c30d3fcd2f8db85b2db0e2495b092c90
Jonathan Ziller <jonathan.ziller@gmail.com>
2015-10-15 09:05:23 +0200

    initial commit

---
+++ Console/Console.fs
@@ -0,0 +1,43 @@
+﻿module Console.State
+
+type State =
+    | AskForShape
+    | AskForCircleRadius
+    | AskForRectangleSideA
+    | AskForRectangleSideB of int64
+    | PrintAnswer of float
+
+let computeCircleArea radius = System.Math.Pow(float radius, 2.0) * System.Math.PI
+let computeRectangleArea a b = float <| a * b
+
+let createWrite writer format = 
+    Printf.kprintf writer format
+
+let stateMachine read writer =
+    let rec loop state =
+        let write = createWrite writer
+        match state with
+        | AskForShape ->
+            write "(C)ircle or (R)ectangle?"
+            match read() with
+            | "C" -> loop AskForCircleRadius
+            | "R" -> loop AskForRectangleSideA
+            | _ -> write "invalid shape"; loop state
+        | AskForCircleRadius ->
+            write "Circle radius?"
+            match System.Int64.TryParse <| read() with
+            | (true, r) when r >= 0L -> loop <| PrintAnswer (computeCircleArea r)
+            | _ -> write "invalid radius"; loop state
+        | AskForRectangleSideA ->
+            write "Rectangle side A length?"
+            match System.Int64.TryParse <| read() with
+            | (true, a) when a >= 0L -> loop <| AskForRectangleSideB a
+            | _ -> write "invalid length"; loop state
+        | AskForRectangleSideB a ->
+            write "Rectangle side B length?"
+            match System.Int64.TryParse <| read() with
+            | (true, b) when b >= 0L -> loop <| PrintAnswer (computeRectangleArea a b)
+            | _ -> write "invalid length"; loop state
+        | PrintAnswer area ->
+            createWrite writer "The area of your shape is: %f" area
+    loop AskForShape
---
+++ Test/Tests.fs
@@ -0,0 +1,54 @@
+﻿namespace Test
+
+open Microsoft.VisualStudio.TestTools.UnitTesting
+open Console.State
+
+type private ExptectedString =
+    | Any
+    | S of string
+
+[<TestClass>]
+type UnitTest() =
+    let createExpectationWriter list =
+        let mutable current = list
+        fun written ->
+            match current with
+            | [] -> ()
+            | c ->
+                let head = List.head c
+                current <- List.tail c
+                match head with
+                | Any -> ()
+                | S s -> 
+                    Assert.AreEqual(s, written)
+            
+    let createReader list =
+        let mutable current = list
+        fun () ->
+            let head = List.head current
+            current <- List.tail current
+            head
+
+    [<TestMethod>]
+    member x.``initially asks for shape`` () =
+        let writer = createExpectationWriter [S "(C)ircle or (R)ectangle?"]
+        let reader = createReader ["C"; "1"]
+        stateMachine reader writer
+
+    [<TestMethod>]
+    member x.``notifies user if input for shape was invalid`` () =
+        let writer = createExpectationWriter [Any; S "invalid shape"]
+        let reader = createReader ["asdasd"; "C"; "1"]
+        stateMachine reader writer
+
+    [<TestMethod>]
+    member x.``asks for shape again if input for shape was invalid`` () =
+        let writer = createExpectationWriter [Any; Any; S "(C)ircle or (R)ectangle?"]
+        let reader = createReader ["asdasd"; "C"; "1"]
+        stateMachine reader writer
+
+    [<TestMethod>]
+    member x.``asks for circle area after user answered shape with "C"`` () =
+        let writer = createExpectationWriter [Any; S "Circle radius?"]
+        let reader = createReader ["C"; "1"]
+        stateMachine reader writer
